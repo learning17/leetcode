9153496d3288eddec3abcedcbc4474cf091d4dab
learning17 <841221809@qq.com>
2021-07-29 23:50:28 +0800

    ci

---
+++ lengthOfLongestSubstring.go
@@ -0,0 +1,26 @@
+package main
+
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+func lengthOfLongestSubstring(s string) int {
+	d := make(map[byte]bool)
+	i, j, maxSize := 0, 0, 0
+	for ; i <= j && j < len(s); {
+		flag, ok := d[s[j]]
+		if !flag || !ok {
+			d[s[j]] = true
+			maxSize = max(maxSize, j-i+1)
+			j++
+		} else {
+			d[s[i]] = false
+			i++
+		}
+	}
+	return maxSize
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
