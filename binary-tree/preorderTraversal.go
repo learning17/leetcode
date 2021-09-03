0f3aba54a4b55099eb3aca35958310ba260ee8a3
learning17 <841221809@qq.com>
2021-09-03 15:35:16 +0800

    二叉树非递归前序遍历

---
+++ binary-tree/preorderTraversal.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	stack := []*TreeNode{}
+	p := root
+	for {
+		for{
+			if p == nil {
+				break
+			}
+			res = append(res, p.Val)
+			stack = append(stack, p)
+			p = p.Left
+		}
+		size := len(stack)
+		if size == 0 {
+			break
+		}
+		node := stack[size-1]
+		stack = stack[:size-1]
+		p = node.Right
+	}
+	return res
+}
