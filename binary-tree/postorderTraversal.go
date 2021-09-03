5dd8e3adeb82737e53ce90b3e744fbe823b663be
learning17 <841221809@qq.com>
2021-09-03 16:21:11 +0800

    二叉树非递归后序遍历

--- binary-tree/postorderTraversal.go
+++ binary-tree/postorderTraversal.go
@@ -1,6 +1,6 @@
 package main
 // https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
-
+// 根——右——左，然后反转得到 左——右——根
 type TreeNode struct {
 	Val int
 	Left *TreeNode
@@ -16,8 +16,9 @@ func postorderTraversal(root *TreeNode) []int {
 			if p == nil {
 				break
 			}
+			res = append(res, p.Val)
 			stack = append(stack, p)
-			p = p.Left
+			p = p.Right
 		}
 		size := len(stack)
 		if size == 0 {
@@ -25,6 +26,10 @@ func postorderTraversal(root *TreeNode) []int {
 		}
 		node := stack[size-1]
 		stack = stack[:size-1]
+		p = node.Left
+	}
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j -1 {
+		res[i], res[j] = res[j],res[i]
 	}
 	return res
 }
