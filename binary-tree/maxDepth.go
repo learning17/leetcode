47a22173bb356c4c50696a59d39b8a9b9ea6ff1f
learning17 <841221809@qq.com>
2021-09-03 17:21:22 +0800

    二叉树的最大深度

---
+++ binary-tree/maxDepth.go
@@ -0,0 +1,20 @@
+package main
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	}
+	return rightDepth + 1
+}
