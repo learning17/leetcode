a64c581c18a38f866ba58dc325cf1bfbc5f7dd34
learning17 <841221809@qq.com>
2021-09-11 12:32:31 +0800

    买卖股票的最佳时机含手续费

---
+++ dynamic-programming/maxProfit_3.go
@@ -0,0 +1,26 @@
+package main
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+func maxProfit(prices []int, fee int) int {
+	size := len(prices)
+	if size == 0 {
+		return 0
+	}
+	dp := make([][]int, size)
+	for i := 0; i < size; i++ {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][1] = -prices[0]
+	for i := 1; i < size; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+	}
+	return dp[size-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
