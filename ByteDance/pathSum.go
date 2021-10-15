4518bdd8c4b42d18e9032b9db9f8f9fad5c8904d
learning17 <841221809@qq.com>
2021-10-15 20:45:14 +0800

    二叉树根节点到叶子节点和为指定值的路径

--- ByteDance/pathSum.go
+++ ByteDance/pathSum.go
@@ -8,5 +8,22 @@ type TreeNode struct {
 }
 
 func pathSum( root *TreeNode ,  sum int ) [][]int {
-
+	var res [][]int
+	var backtrack func(*TreeNode, int, []int)
+	backtrack = func(node *TreeNode, value int, path []int) {
+		if node == nil {
+			return
+		}
+		path = append(path, node.Val)
+		value -= node.Val
+		if node.Left == nil && node.Right == nil && value == 0 {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+		backtrack(node.Left, value, path)
+		backtrack(node.Right, value, path)
+	}
+	var path []int
+	backtrack(root, sum, path)
+	return res
 }
