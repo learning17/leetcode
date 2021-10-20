16cc710444148b6ffa00c875354ad3b8aa4ea23f
learning17 <841221809@qq.com>
2021-10-20 20:12:55 +0800

    最大数

--- ByteDance/solve_4.go
+++ ByteDance/solve_4.go
@@ -5,43 +5,57 @@ package main
 import (
 	"sort"
 	"strconv"
+	"fmt"
 )
+func main() {
+	nums := []int{10,1}
+	res := solve(nums)
+	fmt.Println(res)
+}
+
 func solve( nums []int ) string {
 	help := func(a, b int) bool {
+		if a == 0 {
+			return false
+		}
 		if b == 0 {
 			return true
 		}
 		arr_a, arr_b := []int{}, []int{}
-		for ;a > 0; a /= 10 {
-			arr_a = append(arr_a, a%10)
+		for ; a > 0; a /= 10 {
+			arr_a = append(arr_a, a % 10)
 		}
-		for ;b > 0; b /= 10 {
-			arr_b = append(arr_b, b%10)
+		for ; b > 0; b /= 10 {
+			arr_b = append(arr_b, b % 10)
 		}
 		i, j := len(arr_a)-1, len(arr_b)-1
-		for ; i >=0 && j >= 0; i, j = i-1, j-1 {
+		for ;i >=0 && j >= 0; i, j = i-1, j-1 {
 			if arr_a[i] > arr_b[j] {
 				return true
 			}
+			if arr_a[i] < arr_b[j] {
+				return false
+			}
 		}
-		if i >= 0 && arr_a[i] > arr_b[len(arr_b)-1] {
+		if i >= 0 && arr_a[i] > arr_b[0] {
+			return true
+		}
+		if j >= 0 && arr_b[j] < arr_a[0] {
 			return true
 		}
 		return false
 	}
 	sort.Slice(nums, func(i, j int) bool {return help(nums[i], nums[j])})
-	if len(nums) == 0 {
-		return ""
+	if nums[0] == 0 {
+		return "0"
 	}
 	ans := ""
-	for i := 0; i < len(nums); i++ {
-		if len(ans) == 0 {
-			if nums[i] != 0 {
-				strconv.Itoa(nums[i])
-			}
-			continue
+	for i, num := range nums {
+		if i == 0 {
+			ans = strconv.Itoa(num)
+		} else {
+			ans += strconv.Itoa(num)
 		}
-		ans += strconv.Itoa(nums[i])
 	}
 	return ans
 }
