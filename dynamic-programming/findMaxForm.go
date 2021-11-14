dbbf54466a13e5f91a24a95bcc8bcec239e1329f
learning17 <841221809@qq.com>
2021-11-14 22:12:05 +0800

    01背包

--- dynamic-programming/findMaxForm.go
+++ dynamic-programming/findMaxForm.go
@@ -1,44 +1,36 @@
 package main
 // https://leetcode-cn.com/problems/ones-and-zeroes/discuss/439318/0-1bei-bao/
 
+import (
+	"strings"
+)
+
 func findMaxForm(strs []string, m int, n int) int {
 	size := len(strs)
 	if size == 0 {
 		return 0
 	}
-	dp := make([][][]int, size+1)
-	for k := 0; k < size+1; k++ {
-		dp[k] = make([][]int, m+1)
-		for i := 0; i < m+1; i++ {
-			dp[k][i] = make([]int, n+1)
+	dp := make([][][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([][]int, n+1)
+		for j := 0; j < n+1; j++ {
+			dp[i][j] = make([]int, size+1)
 		}
 	}
-
 	for k := 1; k < size+1; k++ {
-		num_0, num_1 := count(strs[k-1])
-		for i := 0; i < m+1;i++ {
-			for j := 0; j < n + 1; j++ {
-				if num_0 > i || num_1 > j {
-					dp[k][i][j] = dp[k-1][i][j]
+		count0 := strings.Count(strs[k-1], "0")
+		count1 := strings.Count(strs[k-1], "1")
+		for i := 0; i < m+1; i++ {
+			for j := 0; j < n+1; j++ {
+				if count0 > i || count1 > j {
+					dp[i][j][k] = dp[i][j][k-1]
 				} else {
-					dp[k][i][j] = max(dp[k-1][i][j], dp[k-1][i-num_0][j-num_1])
+					dp[i][j][k] = max(dp[i][j][k-1], dp[i-count0][j-count1][k-1]+1)
 				}
 			}
 		}
 	}
-	return dp[size][m][n]
-}
-
-func count(s string)(int, int) {
-	var num_0, num_1 int
-	for _,c := range s {
-		if c == '1' {
-			num_1++
-		} else {
-			num_0++
-		}
-	}
-	return num_0, num_1
+	return dp[m][n][size]
 }
 
 func max(a, b int) int {
@@ -47,3 +39,5 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+
