e5d482c737c90f44e6f06a8e69000e9b41dcf298
learning17 <841221809@qq.com>
2021-11-21 13:00:23 +0800

    买卖股票的最佳时机

--- dynamic-programming/maxProfit.go
+++ dynamic-programming/maxProfit.go
@@ -1,26 +1,20 @@
 package main
 import "math"
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+// 买卖股票的最佳时机
+
 
 func maxProfit(prices []int) int {
-	size := len(prices)
-	if size == 0 {
-		return 0
-	}
-	maxProfit, minPrice := 0, math.MaxInt64
-	for i := 0; i < size; i++ {
-		if minPrice > prices[i] {
+	var minPrice, maxProfit int
+	minPrice = math.MaxInt64
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
 			minPrice = prices[i]
-		} else {
-			maxProfit = max(maxProfit, prices[i] - minPrice)
-		} 
+		}
+		if prices[i] - minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
 	}
 	return maxProfit
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
