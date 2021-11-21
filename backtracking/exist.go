06d99f64f23a0c39b01e3e9fe5f6b9e95073983f
learning17 <841221809@qq.com>
2021-11-21 23:12:00 +0800

    矩阵中的路径

--- backtracking/exist.go
+++ backtracking/exist.go
@@ -1,42 +1,44 @@
 package main
-// https://leetcode-cn.com/problems/word-search/
+// https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+// 矩阵中的路径
 
 func exist(board [][]byte, word string) bool {
-	row_size := len(board)
-	if row_size == 0 {
+	rowSize := len(board)
+	if rowSize == 0 {
 		return false
 	}
-	col_size := len(board[0])
-	visited := make([][]bool, row_size)
-	for i := 0; i < row_size; i++ {
-		visited[i] = make([]bool, col_size)
+	colSize := len(board[0])
+	wordSize := len(word)
+	visited := make([][]bool, rowSize)
+	for i := 0; i < rowSize; i++ {
+		visited[i] = make([]bool, colSize)
 	}
-	word_size := len(word)
-	dire_x, dire_y := []int{0, 0, -1, 1}, []int{-1, 1, 0, 0}
-	var backtrack func(i, j, k int) bool 
-	backtrack = func(i, j, k int) bool {
-		if board[i][j] != word[k] {
+	rDirs := []int{0, 0, -1, 1}
+	cDirs := []int{-1, 1, 0, 0}
+	var backtrack func(r, c, i int) bool
+	backtrack = func(r, c, i int) bool {
+		if board[r][c] != word[i] {
 			return false
 		}
-		if k == word_size - 1 {
+		if i == wordSize - 1 {
 			return true
 		}
-		visited[i][j] = true
-		for l := 0; l < 4; l++{
-			new_x, new_y := i+dire_x[l], j + dire_y[l]
-			if new_x < 0 || new_y < 0 || new_x >= row_size || new_y >= col_size || visited[new_x][new_y] {
+		visited[r][c] = true
+		for k := 0; k < 4; k++ {
+			rn, cn := r+rDirs[k], c+cDirs[k]
+			if rn < 0 || rn >= rowSize || cn < 0 || cn >= colSize || visited[rn][cn] {
 				continue
 			}
-			if backtrack(new_x, new_y, k+1) {
+			if backtrack(rn, cn, i+1) {
 				return true
 			}
 		}
-		visited[i][j] = false
+		visited[r][c] = false
 		return false
 	}
-	for i := 0; i < row_size; i++ {
-		for j := 0; j < col_size; j++ {
-			if backtrack(i, j, 0) {
+	for r := 0; r < rowSize; r++ {
+		for c := 0; c < colSize; c++ {
+			if backtrack(r, c, 0) {
 				return true
 			}
 		}
