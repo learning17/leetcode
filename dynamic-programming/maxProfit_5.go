58de152bfef78636996698a550b0b4446ae04318
learning17 <841221809@qq.com>
2021-11-21 14:46:30 +0800

    最佳买卖股票时机含冷冻期

--- dynamic-programming/maxProfit_5.go
+++ dynamic-programming/maxProfit_5.go
@@ -1,20 +1,23 @@
 package main
-// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+// 最佳买卖股票时机含冷冻期
 
 func maxProfit(prices []int) int {
 	size := len(prices)
 	if size == 0 {
 		return 0
 	}
-	buy1, sell1 := -prices[0], 0
-	buy2, sell2 := -prices[0], 0
-	for i := 1; i < size; i++ {
-		buy1  = max(buy1, -prices[i])
-		sell1 = max(sell1, prices[i] - buy1)
-		buy2  = max(buy2, sell1 - prices[i])
-		sell2 = max(sell2, prices[i] - buy2)
+	dp := make([][]int, size)
+	for i := 0; i < size; i++ {
+		dp[i] = make([]int, 3)
 	}
-	return sell2
+	dp[0][1] = -prices[0]
+	for i := 1; i < size; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][2])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+		dp[i][2] = dp[i-1][1] + prices[i]
+	}
+	return max(dp[size-1][0], dp[size-1][2])
 }
 
 func max(a, b int) int {
