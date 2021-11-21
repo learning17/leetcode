a2c72db46609a602ac91eb39b44cdedb2a351150
learning17 <841221809@qq.com>
2021-11-21 14:29:40 +0800

    买卖股票的最佳时机 IV

--- dynamic-programming/maxProfit_3.go
+++ dynamic-programming/maxProfit_3.go
@@ -1,21 +1,34 @@
 package main
-// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
+// 买卖股票的最佳时机 III
 
-func maxProfit(prices []int, fee int) int {
+func maxProfit(prices []int) int {
+	k := 2
 	size := len(prices)
 	if size == 0 {
 		return 0
 	}
-	dp := make([][]int, size)
+	dp := make([][][]int, size)
 	for i := 0; i < size; i++ {
-		dp[i] = make([]int, 2)
+		dp[i] = make([][]int, k+1)
+		for j := 0; j < k+1; j++ {
+			dp[i][j] = make([]int, 2)
+			dp[0][j][1] = -prices[0]
+		}
 	}
-	dp[0][1] = -prices[0]
 	for i := 1; i < size; i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
-		dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+		for j := 1; j < k+1; j++ {
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i])
+		}
 	}
-	return dp[size-1][0]
+	var maxProfit int
+	for i := 1; i < k+1; i++ {
+		if dp[size-1][k][0] > maxProfit {
+			maxProfit = dp[size-1][k][0]
+		}
+	}
+	return maxProfit
 }
 
 func max(a, b int) int {
@@ -24,3 +37,4 @@ func max(a, b int) int {
 	}
 	return b
 }
+
--- dynamic-programming/maxProfit_4.go
+++ dynamic-programming/maxProfit_4.go
@@ -1,23 +1,33 @@
 package main
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
+// 买卖股票的最佳时机 IV
 
-// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
-
-func maxProfit(prices []int) int {
+func maxProfit(k int, prices []int) int {
 	size := len(prices)
 	if size == 0 {
 		return 0
 	}
-	dp := make([][]int, size)
+	dp := make([][][]int, size)
 	for i := 0; i < size; i++ {
-		dp[i] = make([]int, 3)
+		dp[i] = make([][]int, k+1)
+		for j := 0; j < k+1; j++ {
+			dp[i][j] = make([]int, 2)
+			dp[0][j][1] = -prices[0]
+		}
 	}
-	dp[0][0] = -prices[0]
 	for i := 1; i < size; i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][2] - prices[i])
-		dp[i][1] = dp[i-1][0] + prices[i]
-		dp[i][2] = max(dp[i-1][2], dp[i-1][1])
+		for j := 1; j < k+1; j++ {
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i])
+		}
 	}
-	return max(dp[size-1][1], dp[size-1][2])
+	var maxProfit int
+	for i := 1; i < k+1; i++ {
+		if dp[size-1][k][0] > maxProfit {
+			maxProfit = dp[size-1][k][0]
+		}
+	}
+	return maxProfit
 }
 
 func max(a, b int) int {
@@ -26,3 +36,4 @@ func max(a, b int) int {
 	}
 	return b
 }
+
