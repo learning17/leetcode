5c2b96d561896cd8dd5d30345f80ec1dabc71220
learning17 <841221809@qq.com>
2021-11-22 21:46:03 +0800

    二叉树的深度

--- binary-tree/maxDepth.go
+++ binary-tree/maxDepth.go
@@ -1,20 +1,24 @@
 package main
-// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+// https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+// 二叉树的深度
 
 type TreeNode struct {
 	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
-
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	leftDepth := maxDepth(root.Left)
-	rightDepth := maxDepth(root.Right)
-	if leftDepth > rightDepth {
-		return leftDepth + 1
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	return max(left, right)+1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return rightDepth + 1
+	return b
 }
