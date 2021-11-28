a08bdf59891edfff26e8a62a75262481b2be2c34
learning17 <841221809@qq.com>
2021-11-28 15:01:20 +0800

    无重叠区间

---
+++ greedy/eraseOverlapIntervals.go
@@ -0,0 +1,23 @@
+package main
+// https://leetcode-cn.com/problems/non-overlapping-intervals/
+// 无重叠区间
+import (
+	"sort"
+)
+func eraseOverlapIntervals(intervals [][]int) int {
+	sort.Slice(intervals, func (i, j int) bool {return intervals[i][1] < intervals[j][1]})
+	var num int
+	var tmp []int
+	for i := 0; i < len(intervals); i++ {
+		if i == 0 {
+			tmp = intervals[i]
+			continue
+		}
+		if intervals[i][0] < tmp[1] {
+			num++
+		} else {
+			tmp = intervals[i]
+		}
+	}
+	return num
+}
