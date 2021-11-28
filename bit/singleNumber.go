5cfa80c600284e83016ca9ecb223be2f5afc416a
learning17 <841221809@qq.com>
2021-11-28 16:25:36 +0800

    只出现一次的数字

--- bit/singleNumber.go
+++ bit/singleNumber.go
@@ -1,14 +1,18 @@
 package main
 // https://leetcode-cn.com/problems/WGki4K/
+// 只出现一次的数字 
+
 func singleNumber(nums []int) int {
+	var arr [32]int
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < 32; j++ {
+			arr[j] += (nums[i] >> j) & 1
+		}
+	}
 	var ans int32
 	for i := 0; i < 32; i++ {
-		var total int32
-		for _,num := range nums {
-			total += (int32(num) >> i) & 1
-		}
-		if total % 3 > 0 {
-			ans |= (1 << i)
+		if arr[i] % 3 == 1 {
+			ans |= int32(1 << i)
 		}
 	}
 	return int(ans)
