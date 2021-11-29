e29347cd891e94c738eea7a3c4be92df7bb0fb14
learning17 <841221809@qq.com>
2021-11-29 21:28:42 +0800

    搜索旋转排序数组

---
+++ binary-tree/search.go
@@ -0,0 +1,26 @@
+package main
+//https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+// 搜索旋转排序数组
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right - left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
