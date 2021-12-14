169f848ed9b307fcea9ee3e151987eee2099f616
learning17 <841221809@qq.com>
2021-12-14 21:32:29 +0800

    买卖股票的最佳时机 IV

--- dynamic-programming/maxProfit_3.go
+++ dynamic-programming/maxProfit_3.go
@@ -1,40 +1,5 @@
 package main
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
-// 买卖股票的最佳时机 III
-
-func maxProfit(prices []int) int {
-	k := 2
-	size := len(prices)
-	if size == 0 {
-		return 0
-	}
-	dp := make([][][]int, size)
-	for i := 0; i < size; i++ {
-		dp[i] = make([][]int, k+1)
-		for j := 0; j < k+1; j++ {
-			dp[i][j] = make([]int, 2)
-			dp[0][j][1] = -prices[0]
-		}
-	}
-	for i := 1; i < size; i++ {
-		for j := 1; j < k+1; j++ {
-			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
-			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i])
-		}
-	}
-	var maxProfit int
-	for i := 1; i < k+1; i++ {
-		if dp[size-1][k][0] > maxProfit {
-			maxProfit = dp[size-1][k][0]
-		}
-	}
-	return maxProfit
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
+/* 买卖股票的最佳时机 III
+最多可以完成两笔交易
+*/
--- dynamic-programming/maxProfit_4.go
+++ dynamic-programming/maxProfit_4.go
@@ -1,6 +1,8 @@
 package main
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
-// 买卖股票的最佳时机 IV
+/* 买卖股票的最佳时机 IV
+最多可以完成 k 笔交易
+*/
 
 func maxProfit(k int, prices []int) int {
 	size := len(prices)
@@ -10,21 +12,17 @@ func maxProfit(k int, prices []int) int {
 	dp := make([][][]int, size)
 	for i := 0; i < size; i++ {
 		dp[i] = make([][]int, k+1)
-		for j := 0; j < k+1; j++ {
+		for j :=0; j < k+1; j++ {
 			dp[i][j] = make([]int, 2)
 			dp[0][j][1] = -prices[0]
 		}
 	}
+	var maxProfit int
 	for i := 1; i < size; i++ {
-		for j := 1; j < k+1; j++ {
+		for j := 0; j < k+1; j++ {
 			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
 			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i])
-		}
-	}
-	var maxProfit int
-	for i := 1; i < k+1; i++ {
-		if dp[size-1][k][0] > maxProfit {
-			maxProfit = dp[size-1][k][0]
+			maxProfit = max(maxProfit, dp[i][j][0])
 		}
 	}
 	return maxProfit
@@ -36,4 +34,3 @@ func max(a, b int) int {
 	}
 	return b
 }
-
