06a16e4b79a98c9cee9f2515b9d5ef11f2b543b9
learning17 <841221809@qq.com>
2021-12-14 21:08:35 +0800

    买卖股票的最佳时机

--- dynamic-programming/maxProfit.go
+++ dynamic-programming/maxProfit.go
@@ -1,8 +1,9 @@
 package main
 import "math"
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
-// 买卖股票的最佳时机
-
+/* 买卖股票的最佳时机
+某一天买入这只股票，并选择在未来的某一个不同的日子卖出该股票
+*/
 
 func maxProfit(prices []int) int {
 	var minPrice, maxProfit int
