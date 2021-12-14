0c3a9e987669f0164f70a709cc25221bea5dabb2
learning17 <841221809@qq.com>
2021-12-14 22:15:28 +0800

    跳跃游戏 II

--- dynamic-programming/jump.go
+++ dynamic-programming/jump.go
@@ -1,34 +1,37 @@
 package main
 
 // https://leetcode-cn.com/problems/jump-game-ii/
-
+/*
+跳跃游戏 II
+最初位于数组的第一个位置
+数组中的每个元素代表你在该位置可以跳跃的最大长度
+目标是使用最少的跳跃次数到达数组的最后一个位置
+*/
+import (
+	"math"
+)
 func jump(nums []int) int {
-	size := len(nums)
-	if size == 0 {
-		return -1
-	}
-	dp := make([]int, size)
-	for i := 0; i < size; i++ {
-		dp[i] = size
-	}
-	dp[0] = 0
-	for i := 1; i < size; i++ {
-		for j := 0; j < i; j++ {
-			if nums[j] < i-j {
-				continue
-			}
-			dp[i] = min(dp[i], dp[j]+1)
-		}
-	}
-	if dp[size-1] == size {
-		return -1
-	}
-	return dp[size-1]
+    size := len(nums)
+    if size < 2 {
+        return 0
+    }
+    dp := make([]int, size)
+    for i := 1; i < size; i++ {
+        dp[i] = math.MaxInt64
+    }
+    for i := 1; i < size; i++ {
+        for j := 0; j < i; j++ {
+            if nums[j] >= i - j {
+                dp[i] = min(dp[i], dp[j]+1)
+            }
+        }
+    }
+    return dp[size-1]
 }
 
 func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+    if a < b {
+        return a
+    }
+    return b
 }
