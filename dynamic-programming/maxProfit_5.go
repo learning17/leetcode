663f25fb8269368aee0a9b7693991f848c0cfbde
learning17 <841221809@qq.com>
2021-12-14 22:07:01 +0800

    最佳买卖股票时机含冷冻期

--- dynamic-programming/maxProfit_5.go
+++ dynamic-programming/maxProfit_5.go
@@ -1,6 +1,8 @@
 package main
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
-// 最佳买卖股票时机含冷冻期
+/* 最佳买卖股票时机含冷冻期
+卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)
+*/
 
 func maxProfit(prices []int) int {
 	size := len(prices)
@@ -14,8 +16,8 @@ func maxProfit(prices []int) int {
 	dp[0][1] = -prices[0]
 	for i := 1; i < size; i++ {
 		dp[i][0] = max(dp[i-1][0], dp[i-1][2])
-		dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
-		dp[i][2] = dp[i-1][1] + prices[i]
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+		dp[i][2] = dp[i-1][1]+prices[i]
 	}
 	return max(dp[size-1][0], dp[size-1][2])
 }
