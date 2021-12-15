0042660532236ec304d81bb27d04595bc0988812
learning17 <841221809@qq.com>
2021-12-15 11:46:17 +0800

    子集背包:分割等和子集

--- dynamic-programming/canPartition.go
+++ dynamic-programming/canPartition.go
@@ -1,16 +1,14 @@
 package main
 // https://leetcode-cn.com/problems/partition-equal-subset-sum/
+/*
+子集背包:分割等和子集
+给你一个只包含正整数的非空数组nums 。
+请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等
+*/
 
 func canPartition(nums []int) bool {
-	size := len(nums)
-	if size == 0 {
-		return false
-	}
-	var sum int
-	for _, num := range nums {
-		sum += num
-	}
-	if sum % 2 != 0 {
+	sum, size := calSum(nums), len(nums)
+	if sum % 2 != 0 || size == 0 {
 		return false
 	}
 	half := sum / 2
@@ -20,7 +18,7 @@ func canPartition(nums []int) bool {
 		dp[i][0] = true
 	}
 	for i := 1; i < size+1; i++ {
-		for j := 1; j < half+1; j++ {
+		for j := 0; j < half+1; j++ {
 			if j < nums[i-1] {
 				dp[i][j] = dp[i-1][j]
 			} else {
@@ -29,6 +27,14 @@ func canPartition(nums []int) bool {
 		}
 	}
 	return dp[size][half]
+}
+
+func calSum(nums []int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
 }
 
 
