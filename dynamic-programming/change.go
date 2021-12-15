86936d9871029c1e55f1ed5a1b5073aa872b26ac
learning17 <841221809@qq.com>
2021-12-15 11:30:54 +0800

    完全背包:零钱兑换 II

--- dynamic-programming/change.go
+++ dynamic-programming/change.go
@@ -1,5 +1,11 @@
 package main
 // https://leetcode-cn.com/problems/coin-change-2/
+/*
+完全背包:零钱兑换 II
+给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额
+请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0
+假设每一种面额的硬币有无限个
+*/
 
 func change(amount int, coins []int) int {
 	size := len(coins)
@@ -11,7 +17,6 @@ func change(amount int, coins []int) int {
 		dp[i] = make([]int, amount+1)
 		dp[i][0] = 1
 	}
-
 	for i := 1; i < size+1; i++ {
 		for j := 1; j < amount+1; j++ {
 			if j < coins[i-1] {
