8c3a9e25e217808f5da7e6c56bf2af832b82c6f0
learning17 <841221809@qq.com>
2021-12-15 12:07:58 +0800

    鸡蛋掉落

--- dynamic-programming/superEggDrop.go
+++ dynamic-programming/superEggDrop.go
@@ -1,5 +1,11 @@
 package main
 // https://leetcode-cn.com/problems/super-egg-drop/
+/*
+鸡蛋掉落
+给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
+已知存在楼层 f ，满足 0 <= f <= n ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
+请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少
+*/
 
 import (
 	"math"
