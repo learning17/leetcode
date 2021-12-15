a91c740a9748570a6504fcfc587893318d1338bf
learning17 <841221809@qq.com>
2021-12-15 11:15:49 +0800

    01 背包:一和零

--- dynamic-programming/findMaxForm.go
+++ dynamic-programming/findMaxForm.go
@@ -1,5 +1,10 @@
 package main
 // https://leetcode-cn.com/problems/ones-and-zeroes/discuss/439318/0-1bei-bao/
+/*
+01 背包（当前物品拿或者不拿，每个物品被限定只能选择0个或1个）：一和零
+给你一个二进制字符串数组 strs 和两个整数 m 和 n
+请你找出并返回strs的最大子集的长度，该子集中最多有 m 个 0 和 n 个 1
+*/
 
 import (
 	"strings"
@@ -10,27 +15,27 @@ func findMaxForm(strs []string, m int, n int) int {
 	if size == 0 {
 		return 0
 	}
-	dp := make([][][]int, m+1)
-	for i := 0; i < m+1; i++ {
-		dp[i] = make([][]int, n+1)
-		for j := 0; j < n+1; j++ {
-			dp[i][j] = make([]int, size+1)
+	dp := make([][][]int, size+1)
+	for i := 0; i < size+1; i++ {
+		dp[i] = make([][]int, m+1)
+		for j := 0; j < m+1; j++ {
+			dp[i][j] = make([]int, n+1)
 		}
 	}
-	for k := 1; k < size+1; k++ {
-		count0 := strings.Count(strs[k-1], "0")
-		count1 := strings.Count(strs[k-1], "1")
-		for i := 0; i < m+1; i++ {
-			for j := 0; j < n+1; j++ {
-				if count0 > i || count1 > j {
-					dp[i][j][k] = dp[i][j][k-1]
+
+	for i := 1; i < size+1; i++ {
+		num0, num1 := strings.Count(strs[i-1], "0"), strings.Count(strs[i-1], "1") 
+		for j := 0; j < m+1; j++ {
+			for k := 0; k < n+1; k++ {
+				if num0 > j || num1 > k {
+					dp[i][j][k] = dp[i-1][j][k]
 				} else {
-					dp[i][j][k] = max(dp[i][j][k-1], dp[i-count0][j-count1][k-1]+1)
+					dp[i][j][k] = max(dp[i-1][j][k], dp[i-1][j-num0][k-num1]+1)
 				}
 			}
 		}
 	}
-	return dp[m][n][size]
+	return dp[size][m][n]
 }
 
 func max(a, b int) int {
@@ -39,5 +44,4 @@ func max(a, b int) int {
 	}
 	return b
 }
-
 
