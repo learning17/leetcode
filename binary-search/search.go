9b5db014a7b54766685e1c8ec604f73be38b75a9
learning17 <841221809@qq.com>
2021-12-16 20:09:15 +0800

    二分查找

--- binary-search/search.go
+++ binary-search/search.go
@@ -1,21 +1,24 @@
 package main
 
 // https://leetcode-cn.com/problems/binary-search/
+/*
+二分查找
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1
+*/
 
 func search(nums []int, target int) int {
-	left, right := 0, len(nums) - 1
-	for {
-		if left > right {
-			return -1
-		}
-		mid := left + (right - left)/2
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] > target {
-			right = mid - 1
-		} else {
+		} else if nums[mid] < target {
 			left = mid+1
+		} else {
+			right = mid-1
 		}
 	}
+	return -1
 }
 
