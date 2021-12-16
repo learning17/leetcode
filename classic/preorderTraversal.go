06b322b572a661df5fdde2accf00703677973683
learning17 <841221809@qq.com>
2021-12-16 21:56:15 +0800

    二叉树的前序非递归遍历

--- classic/preorderTraversal.go
+++ classic/preorderTraversal.go
@@ -1,5 +1,9 @@
 package main
 // https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+/*
+二叉树的前序非递归遍历
+按照访问根节点——左子树——右子树的方式遍历这棵树
+*/
 
 type TreeNode struct {
 	Val int
@@ -15,8 +19,9 @@ func preorderTraversal(root *TreeNode) []int {
 			stack = append(stack, root)
 			root = root.Left
 		}
-		root = stack[len(stack)-1].Right
+		root = stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
+		root = root.Right
 	}
 	return res
 }
