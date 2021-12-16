338ba4344e26d593dde3765e942619c06cefa2d4
learning17 <841221809@qq.com>
2021-12-16 21:03:56 +0800

    0～n-1中缺失的数字

--- binary-search/missingNumber.go
+++ binary-search/missingNumber.go
@@ -1,17 +1,18 @@
 package main
 // https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+/*
+0～n-1中缺失的数字
+*/
 
 func missingNumber(nums []int) int {
-	left, right := 0, len(nums)
-	for {
-		if left == right {
-			return left
-		}
-		mid := left + (right - left)/2
+	left, right := 0, len(nums) - 1
+	for left <= right {
+		mid := left + (right - left) / 2
 		if nums[mid] == mid {
-			left = mid+1
+			left = mid + 1
 		} else {
-			right = mid
+			right = mid - 1
 		}
 	}
+	return left
 }
