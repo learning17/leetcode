d4230df4c31835cab4d3de86b5522b36d1d58396
learning17 <841221809@qq.com>
2021-12-16 21:14:46 +0800

    算术平方根

--- binary-search/mySqrt.go
+++ binary-search/mySqrt.go
@@ -1,21 +1,19 @@
 package main
 // https://leetcode-cn.com/problems/sqrtx/
-
+/*
+算术平方根
+*/
 func mySqrt(x int) int {
-	left, right, sqrt := 0, x, 0
-	for {
-		if left > right {
-			return sqrt
-		}
+	left, right := 0, x
+	for left <= right {
 		mid := left + (right - left)/2
-		tmp := mid * mid
-		if tmp == x {
+		if mid * mid == x {
 			return mid
-		} else if tmp < x {
-			sqrt = mid
-			left = mid + 1
-		} else {
+		} else if mid * mid > x {
 			right = mid - 1
+		} else {
+			left = mid + 1
 		}
 	}
+	return left-1
 }
