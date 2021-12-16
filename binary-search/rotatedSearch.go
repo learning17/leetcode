da6e6a4434b235e42b4b31cc0c978a41777f2e13
learning17 <841221809@qq.com>
2021-12-16 20:44:38 +0800

    搜索旋转排序数组

--- binary-search/rotatedSearch.go
+++ binary-search/rotatedSearch.go
@@ -1,31 +1,30 @@
 package main
-// https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
+// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+/*
+搜索旋转排序数组
+数组中的值互不相同
+*/
 
-func search(nums []int, target int) bool {
-	left, right := 0, len(nums) - 1
-	for {
-		if left > right {
-			return false
-		}
-		mid := left + (right-left)/2
-		if nums[mid] == target {
-			return true
-		}
-		if nums[left] == nums[mid] && nums[mid] == nums[right] {
-			left++
-			right--
-		} else if nums[left] <= nums[mid] {
-			if nums[left] <= target && target < nums[mid] {
-				right = mid-1
-			} else {
-				left = mid+1
-			}
-		} else {
-			if nums[mid] < target && target <= nums[right] {
-				left = mid+1
-			} else {
-				right = mid-1
-			}
-		}
-	}
+func search(nums []int, target int) int {
+    left, right := 0, len(nums)-1
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[left] <= nums[mid] {
+            if nums[left] <= target && target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid+1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return -1
 }
