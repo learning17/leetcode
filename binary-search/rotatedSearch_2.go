867cb7164c6a0bf6e71823eaf66d9717a2bd6921
learning17 <841221809@qq.com>
2021-12-16 20:56:27 +0800

    搜索旋转排序数组 II

--- binary-search/rotatedSearch_2.go
+++ binary-search/rotatedSearch_2.go
@@ -1,13 +1,13 @@
 package main
 // https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
-
+/*
+搜索旋转排序数组 II
+数组中的值不必互不相同
+*/
 func search(nums []int, target int) bool {
 	left, right := 0, len(nums) - 1
-	for {
-		if left > right {
-			return false
-		}
-		mid := left + (right-left)/2
+	for left <= right {
+		mid := left + (right - left)/2
 		if nums[mid] == target {
 			return true
 		}
@@ -16,16 +16,17 @@ func search(nums []int, target int) bool {
 			right--
 		} else if nums[left] <= nums[mid] {
 			if nums[left] <= target && target < nums[mid] {
-				right = mid-1
+				right = mid - 1
 			} else {
-				left = mid+1
+				left = mid + 1
 			}
 		} else {
 			if nums[mid] < target && target <= nums[right] {
-				left = mid+1
+				left = mid + 1
 			} else {
-				right = mid-1
+				right = mid - 1
 			}
 		}
 	}
+	return false
 }
