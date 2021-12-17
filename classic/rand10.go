10a0d4ab8a5228ca8cbb1db81a2e527e0bb27a12
learning17 <841221809@qq.com>
2021-12-17 21:22:35 +0800

    用 Rand7() 实现 Rand10()

--- classic/rand10.go
+++ classic/rand10.go
@@ -1,14 +1,25 @@
 package main
 // https://leetcode-cn.com/problems/implement-rand10-using-rand7/
-// 用 Rand7() 实现 Rand10()
+/*
+用 Rand7() 实现 Rand10()
+(randX-1)*Y+randY 可以等概率生成[1, XY] 的随机数
+*/
+import (
+	"math/rand"
+	"time"
+)
+
 func rand10() int {
-	res := 41
-	for res > 40 {
-		res = (rand7() - 1) * 7 + rand7()
+	for {
+		idx := (rand7()-1)*7 + rand7()
+		if idx <= 40 {
+			return 1 + (idx-1) % 10
+		}
 	}
-	return 1 + (res-1) % 10
 }
 
 func rand7() int {
-	return 1
+	rand.Seed(int64(time.Millisecond))
+	return rand.Intn(7)
 }
+
