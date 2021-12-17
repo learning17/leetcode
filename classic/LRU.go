c448f915b45968bbdd7f2b933e64a3004451c7a6
learning17 <841221809@qq.com>
2021-12-17 12:03:28 +0800

    LRU

--- classic/LRU.go
+++ classic/LRU.go
@@ -1,14 +1,14 @@
 package main
 // https://leetcode-cn.com/problems/OrIXps/
 
-// LRU 基于container 包
+// 基于 container/list 实现 LRU
 import (
 	"container/list"
 )
 
 type Node struct {
-	key int
-	value int
+	Key int
+	Val int
 }
 
 type LRUCache struct {
@@ -19,7 +19,7 @@ type LRUCache struct {
 }
 
 func Constructor(capacity int) LRUCache {
-	return LRUCache {
+	return LRUCache{
 		capacity: capacity,
 		dict: make(map[int]*list.Element),
 		linkList: list.New(),
@@ -27,30 +27,31 @@ func Constructor(capacity int) LRUCache {
 }
 
 func (this *LRUCache) Get(key int) int {
-	node, ok := this.dict[key]
+	e, ok := this.dict[key]
 	if !ok {
 		return -1
 	}
-	this.linkList.MoveToFront(node)
-	return node.Value.(*Node).value
+	this.linkList.MoveToFront(e)
+	return e.Value.(*Node).Val
 }
 
-func (this *LRUCache) Put(key int, value int) {
-	node, ok := this.dict[key]
+func (this *LRUCache) Put(key int, value int)  {
+	e, ok := this.dict[key]
 	if ok {
-		p, _ := node.Value.(*Node)
-		p.value = value
-		this.linkList.MoveToFront(node)
+		this.linkList.MoveToFront(e)
+		node := e.Value.(*Node)
+		node.Val = value
 		return
 	}
-	if this.size == this.capacity {
-		node = this.linkList.Back()
-		delete(this.dict, node.Value.(*Node).key)
-		this.linkList.Remove(node)
+	if this.capacity == this.size {
+		e = this.linkList.Back()
+		this.linkList.Remove(e)
+		delete(this.dict, e.Value.(*Node).Key)
 		this.size--
 	}
 	this.size++
-	this.dict[key] = this.linkList.PushFront(&Node{key, value})
+	node := &Node{key, value}
+	this.dict[key] = this.linkList.PushFront(node)
 }
 
 /*
@@ -59,7 +60,7 @@ type LinkNode struct {
 	Value int
 	Pre *LinkNode
 	Next *LinkNode
-}
+} 
 
 type LinkList struct {
 	head *LinkNode
@@ -72,7 +73,7 @@ func (this *LinkList) MoveToFront(node *LinkNode) {
 	}
 	if node.Next == nil {
 		this.tail = node.Pre
-		node.Pre.Next, node.Pre = nil, nil
+		node.Pre.Next, node.Pre = nil, nil 
 	} else {
 		node.Pre.Next, node.Next.Pre = node.Next, node.Pre
 		node.Pre, node.Next = nil, nil
@@ -95,14 +96,14 @@ func (this *LinkList) Remove(node *LinkNode) {
 		this.tail = node.Pre
 	}
 	node.Pre, node.Next = nil, nil
-} 
+}
 
 func (this *LinkList) PushFront(node *LinkNode) {
 	if this.head == nil {
 		this.head, this.tail = node, node
 		return
 	}
-	this.head.Pre, node.Next = node, this.head
+	node.Next, this.head.Pre = this.head, node
 	this.head = node
 }
 
@@ -130,23 +131,21 @@ func (this *LRUCache) Get(key int) int {
 	return node.Value
 }
 
-func (this *LRUCache) Put(key int, value int) {
+func (this *LRUCache) Put(key int, value int)  {
 	node, ok := this.dict[key]
 	if ok {
 		node.Value = value
 		this.linkList.MoveToFront(node)
 		return
 	}
-	if this.size == this.capacity {
-		delete(this.dict, this.linkList.tail.Key)
-		this.linkList.Remove(this.linkList.tail)
+	if this.capacity == this.size {
+		node := this.linkList.tail
+		delete(this.dict, node.Key)
+		this.linkList.Remove(node)
 		this.size--
 	}
 	this.size++
-	node = &LinkNode{
-		Key: key,
-		Value: value,
-	}
-	this.dict[key] = node
+	node = &LinkNode{key, value, nil, nil}
 	this.linkList.PushFront(node)
+	this.dict[key] = node
 }*/
