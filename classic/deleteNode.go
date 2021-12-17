91254fd3ec9fd703186df170f05d4feeec334123
learning17 <841221809@qq.com>
2021-12-17 20:44:11 +0800

    删除二叉搜索树中的节点

--- classic/deleteNode.go
+++ classic/deleteNode.go
@@ -1,19 +1,23 @@
 package main
 // https://leetcode-cn.com/problems/delete-node-in-a-bst/
+/*
+删除二叉搜索树中的节点
+*/
 
 type TreeNode struct {
 	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
+
 func deleteNode(root *TreeNode, key int) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if root.Val < key {
-		root.Right = deleteNode(root.Right, key)
-	} else if root.Val > key {
+	if root.Val > key {
 		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
 	} else {
 		if root.Left == nil {
 			return root.Right
@@ -21,14 +25,14 @@ func deleteNode(root *TreeNode, key int) *TreeNode {
 		if root.Right == nil {
 			return root.Left
 		}
-		minNode := findMin(root.Right)
+		minNode := findMinNode(root.Right) 
 		root.Val = minNode.Val
 		root.Right = deleteNode(root.Right, minNode.Val)
 	}
 	return root
 }
 
-func findMin(node *TreeNode) *TreeNode {
+func findMinNode(node *TreeNode) *TreeNode {
 	for node.Left != nil {
 		node = node.Left
 	}
