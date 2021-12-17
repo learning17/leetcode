a47e4a3754c9d1aa5b675764cc44b65290db489d
learning17 <841221809@qq.com>
2021-12-17 21:46:19 +0800

    路径总和 III

--- binary-tree/pathSum.go
+++ binary-tree/pathSum.go
@@ -1,6 +1,10 @@
 package main
 // https://leetcode-cn.com/problems/path-sum-iii/
-// 路径总和 III
+/*
+路径总和 III
+给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目
+路径 不需要从根节点开始，也不需要在叶子节点结束
+*/
 type TreeNode struct {
 	Val int
 	Left *TreeNode
@@ -21,9 +25,9 @@ func pathSum(root *TreeNode, targetSum int) int {
 		if target - node.Val == 0 {
 			num++
 		}
-		backtrack(node.Left, target - node.Val)
-		backtrack(node.Right, target - node.Val)
+		backtrack(node.Left, target- node.Val)
+		backtrack(node.Right, target- node.Val)
 	}
 	backtrack(root, targetSum)
-	return num + left + right
+	return left+right+num
 }
